744bc91d0ebdea7d5562571d479b3090f018058c
Vishalku88 <164476507+Vishalku88@users.noreply.github.com>
2024-03-24 22:34:42 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/VJBots/Advance-Auto-Filter /Advance-Auto-Filter
+  git clone https://github.com/Vishalku88/Vk-Auto-Filter/tree/master /Vk-Auto-Filter
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Advance-Auto-Filter
+  git clone $UPSTREAM_REPO /Vk-Auto-Filter
 fi
-cd /Advance-Auto-Filter
+cd /Vk-Auto-Filter
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
